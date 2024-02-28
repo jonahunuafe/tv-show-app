61116e94064b286dfcd914079306f615050aa5c3
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-28 14:56:16 +0100

    added main navigation component

---
+++ src/components/MainNavigation.js
@@ -0,0 +1,18 @@
+function MainNavigation() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        Tv Show
+                    </li>
+                    <li>
+                        Tv Episode
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default MainNavigation;
