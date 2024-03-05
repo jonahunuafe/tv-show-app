b8a8c568e55981b9f906845b4f5ec1ce7a1219bf
Jonah Unuafe <fejisfejiro@gmail.com>
2024-03-05 17:29:06 +0100

    updated index.css

--- src/pages/EpisodeDetailsPage.js
+++ src/pages/EpisodeDetailsPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { useEffect, useState } from "react";
 
 
@@ -11,9 +10,7 @@ function EpisodeDetailsPage() {
     const fetchInfo = async () => {
         const response = await fetch(url);
         const resData = await response.json();
-
         setData(resData);
-
     }
 
     useEffect(() => {
@@ -21,17 +18,19 @@ function EpisodeDetailsPage() {
     });
 
     return (
-        <ul className="show-data">
-            {data.map((tvData) => (
-                <li className="datas-list" key={tvData.id}>
-                    <img src={tvData.image.original} alt="" />
-                    <div className="mapped-text">
-                        <h1>{tvData.name}</h1>
-                        <p>{tvData.summary.replace(/<[^>]+>/g, '')}</p>
-                    </div>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className="show-data">
+                    {data.map((tvData) => (
+                        <li className="datas-list" key={tvData.id}>
+                            <img src={tvData.image.original} alt="" />
+                            <div className="mapped-text">
+                                <h1>{tvData.name}</h1>
+                                <p>{tvData.summary.replace(/<[^>]+>/g, '')}</p>
+                            </div>
+                        </li>
+                    ))}
+            </ul>
+        </>
     );
 }
 
