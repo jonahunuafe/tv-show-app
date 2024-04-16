0834c75aad2c831325c4440b790ba3178c3ac4f7
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-16 13:48:08 +0100

    implemented local storage

--- src/components/EpisodeDetails.js
+++ src/components/EpisodeDetails.js
@@ -1,38 +1,45 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState, useCallback } from "react";
+// import { useParams } from "react-router-dom";
 import Header from "./Header";
 import EpisodeDetail from "./EpisodeDetail";
 import classes from "./EpisodeDetails.module.css"
 
 function EpisodeDetails() {
-    const [data, setData] = useState([]);
+    // const { id } = useParams();
+    const [showData, setShowData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
     
     const url = "https://api.tvmaze.com/shows";
 
-    const fetchInfo = async () => {
+    useEffect(() => {
+        const data = localStorage.getItem('list');
+        if ( data !== null ) setShowData(JSON.parse(data));
+    }, []);
+
+    const fetchInfo = useCallback(async () => {
         try {
             const response = await fetch(url);
             const resData = await response.json();
-            setData(resData);
+            localStorage.setItem('list', JSON.stringify(showData));
+            setShowData(resData);
         } catch(error) {
             setError("Error fetching data!", error);
         } finally {
             setLoading(false);
         }
-    }
+    }, [showData])
 
     useEffect(() => {
         fetchInfo();
-    }, []);
+    }, [fetchInfo]);
 
     return (
         <>
             <Header />
             {loading ? (<h2>Loading...</h2>) : (
                 <div className={classes.data}>
-                    {data.map((tvData) => (
+                    {showData.map((tvData) => (
                         <EpisodeDetail tvData={tvData} key={tvData.id} />
                     ))}
                 </div>)
