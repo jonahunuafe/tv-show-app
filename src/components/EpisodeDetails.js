9fd47291a8179688bc7ca689185271ab1d57ca9b
Jonah Unuafe <fejisfejiro@gmail.com>
2024-04-16 14:50:19 +0100

    removed local storage

--- src/components/EpisodeDetails.js
+++ src/components/EpisodeDetails.js
@@ -1,38 +1,30 @@
-import React, { useEffect, useState, useCallback } from "react";
-// import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import EpisodeDetail from "./EpisodeDetail";
 import classes from "./EpisodeDetails.module.css"
 
 function EpisodeDetails() {
-    // const { id } = useParams();
     const [showData, setShowData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
     
     const url = "https://api.tvmaze.com/shows";
 
-    useEffect(() => {
-        const data = localStorage.getItem('list');
-        if ( data !== null ) setShowData(JSON.parse(data));
-    }, []);
-
-    const fetchInfo = useCallback(async () => {
+    const fetchInfo = async () => {
         try {
             const response = await fetch(url);
             const resData = await response.json();
-            localStorage.setItem('list', JSON.stringify(showData));
             setShowData(resData);
         } catch(error) {
             setError("Error fetching data!", error);
         } finally {
             setLoading(false);
         }
-    }, [showData])
+    };
 
     useEffect(() => {
         fetchInfo();
-    }, [fetchInfo]);
+    }, []);
 
     return (
         <>
